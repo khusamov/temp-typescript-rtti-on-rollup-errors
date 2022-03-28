6f4cd69558e47dc315660f547f86377d10a8a4a6
William Lahti <wilahti@gmail.com>
2022-03-28 16:49:06 -0700

    workaround

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,2 @@
 import "reflect-metadata"
-import {reflect} from 'typescript-rtti'
-import IMovable from './IMovable';
-
-// An array with two properties is expected to be output.
-// But instead an empty array is output!
-console.log(
-	reflect<IMovable>().as('interface').reflectedInterface.properties
-)
+import "./root";
